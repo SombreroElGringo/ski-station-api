ba96a0448bb7cb206dd39e517b08d9d69b2cdb25
SombreroElGringo <florent.pailhes@hotmail.fr>
2019-01-25 23:06:50 +0100

    Migrate DB to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_01_25_204502) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stations", force: :cascade do |t|
     t.string "code"
     t.string "name"
